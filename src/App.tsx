84df2c93da47e7bbf62d2f0e6b95b61bc55c6396
MatthewBart <MatthewCBartley@gmail.com>
2023-03-01 16:53:19 +0000

    Array prints to screen, Pressing + inserts a new box into the array

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function changeCell(newCellVal: string, idxToChange : number)
+  {
+    setCells((prevCells) =>
+      prevCells.map((cell, i)=>
+      (i === idxToChange?newCellVal:cell))
+      );
+  }
+  const [cells,setCells] = useState(["a","b","c"]);
+
+  function handleClicked(idx: number){
+    setCells((prevCells)=>[...prevCells.slice(0,idx + 1)," ",...prevCells.slice(idx+1)]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="cells">
+        {cells.map((cell, idx) =>(
+          <div key={idx} className = "cell">
+            <input 
+            onChange={(e) => changeCell(e.currentTarget.value, idx)} 
+            value={cell}>
+            </input>
+          {idx<cells.length-1&&
+          <span onClick={() => handleClicked(idx)}>+</span>}
+          </div>
+        ))}
+      </div>    
     </div>
   );
 }
